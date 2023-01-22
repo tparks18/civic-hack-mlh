65cfa4cdbfaaf2b7e305049b4c63784615f21a40
Taty <tatyanaparks@gmail.com>
2023-01-21 22:33:33 -0600

    added to about and resources

--- app/routes.py
+++ app/routes.py
@@ -10,3 +10,8 @@ def index():
 def about():
     title = 'About'
     return render_template('about.html', title=title)
+
+@app.route('/resources')
+def resources():
+    title = 'Resources'
+    return render_template('resources.html', title=title)

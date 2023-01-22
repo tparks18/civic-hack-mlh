18f3676f37f0d280a7682393b0ce63d9a17561d9
Taty <tatyanaparks@gmail.com>
2023-01-21 22:09:18 -0600

    first commit

---
+++ app/routes.py
@@ -0,0 +1,12 @@
+from app import app
+from flask import redirect, url_for, render_template
+
+
+@app.route('/')
+def index():
+    return redirect(url_for('inventory.index'))
+
+@app.route('/about')
+def about():
+    title = 'About'
+    return render_template('about.html', title=title)
